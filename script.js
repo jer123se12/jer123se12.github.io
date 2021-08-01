2a146096235edfdc11f2582f028ba0195e585e30
jer123se12 <64419693+jer123se12@users.noreply.github.com>
2021-08-01 17:55:24 +0800

    template more or less done

--- script.js
+++ script.js
@@ -170,3 +170,8 @@ function generation(points,l){
 
     return l2
 }
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
