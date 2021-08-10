b8ce420247125bc11c90fe15069b544c67c02006
jer123se12 <64419693+jer123se12@users.noreply.github.com>
2021-08-10 13:42:26 +0800

    new version
    yay

---
+++ old ver/script.js
@@ -0,0 +1,190 @@
+let did=0
+let dob=new Date(2006,03,28)
+var running=true
+function load(){
+    
+    document.getElementById("school").innerHTML="School of Science and Technology Singapore"
+    if (window.innerWidth>600){
+        desktop()
+    }else{
+        mobile()
+    }
+    draw()
+}
+function mobile(){
+    document.getElementById("mobile-navbar").style.display="block"
+    document.getElementById("mobile-navbar").style.listStyleType = "none";
+    items=document.getElementsByClassName("navitem")
+    start=0
+    for (let i=0;i<items.length;i++){
+        items[i].style.float="none"
+        items[i].style.display="none"
+    }
+    
+    document.getElementById("mm").style.display="block"
+    document.getElementById("mm").style.float="right"
+}
+function show(){
+    // console.log("hit")
+    items=document.getElementsByClassName("navitem")
+    if (items[1].style.display=="none"){
+        for (let i=0;i<items.length;i++){
+        items[i].style.display="block"
+        }
+        start=0
+        id=setInterval(frame,10)
+        function frame(){
+        if (start>=2){
+            clearInterval(id)
+        }
+        start+=0.1
+        // console.log(start)
+        for (let i=0;i<items.length;i++){
+            items[i].style.top=toString(start)+"rem"
+            // console.log(items[i].style.height)
+        }
+        }
+    }else{
+        for (let i=0;i<items.length;i++){
+            items[i].style.display="none"
+            }
+            document.getElementById("mm").style.display="block"
+    }
+}
+
+function desktop(){
+    items=document.getElementsByClassName("navitem")
+    for (let i=0;i<items.length;i++){
+        items[i].style.display="block"
+        items[i].style.float="right"
+    }
+    document.getElementById("mm").style.display="none"
+
+}
+
+
+
+
+function draw(){
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.canvas.width  =innerWidth;
+    ctx.canvas.height = innerHeight;
+    last=0
+    var x=0
+    var y=0
+    var s=10
+    var arr = Array(Math.floor((innerHeight/s)+2)).fill().map(() => Array(Math.floor((innerWidth/s)+2)).fill().map(() => 1));
+    var ps=[]
+    ps=[[Math.floor(Math.random()*arr.length),Math.floor( Math.random()*arr[0].length)]]
+    for (var i=0;i<Math.floor(Math.random()*arr.length)+10;i++){
+        ps.push([Math.floor(Math.random()*arr[0].length),Math.floor( Math.random()*arr.length)])
+    }
+    var directions=[]
+    for (var i=0;i<ps.length;i++){
+        directions.push([Math.floor((Math.random()-0.5)*30)/10,Math.floor((Math.random()-0.5)*30)/10])
+    }
+    // document.addEventListener("click", addpos);
+    window.requestAnimationFrame(gameLoop);
+    // function addpos(event){
+    //     // arr[Math.floor(event.clientY/s)][Math.floor(event.clientX/s)]=1
+    //     // console.log(arr)
+    //     x=Math.floor(event.clientX/s)
+    //     y=Math.floor(event.clientY/s)
+    //     ps.push([x,y])
+
+    // }
+    function gameLoop(ts){
+        let time=new Date();
+        document.getElementById("age").innerHTML = Math.floor((time.getTime()-dob.getTime())/315.36)/100000000
+
+
+        if (last != Math.floor(ts/50)){
+            last=Math.floor(ts/50)
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "hsl(239, 100%, 17%)";
+        ctx.fillRect(0,0,canvas.width, canvas.height)
+
+        arr=generation(ps,arr)
+        
+        for (var i=0;i<arr.length;i++){
+            for (var j=0;j<arr[i].length;j++){
+                if (arr[i][j]>0){
+                ctx.fillStyle = `hsl(
+                    ${/*Math.floor(arr[i][j]*255)*/ 239},
+                    ${(100)-Math.floor(arr[i][j]*(10*s)) }%,
+                    ${(40)+Math.floor(arr[i][j]*2.1*s)}%)`;
+                // ctx.fillRect((j-1)*s,(i-1)*s,s,s)
+                ctx.beginPath()
+                ctx.arc((j-1)*s, (i-1)*s, s/2, 0, 2 * Math.PI, false);
+                ctx.fill()
+                ctx.closePath();
+            }
+            }
+        }
+        for (var i=0;i<directions.length;i++){
+            ps[i]=[ps[i][0]+directions[i][0],ps[i][1]+directions[i][1]]
+            if (ps[i][0]-2<0 || ps[i][0]+2>arr[0].length){
+                directions[i][0]=directions[i][0]*-1
+            }
+            if (ps[i][1]-2<0 || ps[i][1]+2>arr.length){
+                directions[i][1]=directions[i][1]*-1
+            }
+            
+        }
+
+
+    }        
+        window.requestAnimationFrame(gameLoop);
+    }
+}
+
+function generation(points,l){
+    // l3=l.map(x => x.map(y => Math.floor(y*90)/100))
+    // l2=l.map(x => x.map(y => Math.floor(y*90)/100))
+    // for (var i=0;i<l.length;i++){
+    //     for (var j=0;j<l[i].length;j++){
+    //         if (l[i][j]>0){
+    //             if (i+1<l.length){
+    //                 l2[i+1][j]=(l2[i][j]>l3[i+1][j]) ? l2[i][j]:l3[i+1][j]
+    //             }
+    //             if (i-1>0){
+    //                 l2[i-1][j]=(l2[i][j]>l3[i-1][j]) ? l2[i][j]:l3[i-1][j]
+    //             }
+    //             if (j+1<l[i].length){
+    //                 l2[i][j+1]=(l2[i][j]>l3[i][j+1]) ? l2[i][j]:l3[i][j+1]
+    //             }
+    //             if (j-1>0){
+    //                 l2[i][j-1]=(l2[i][j]>l3[i][j-1]) ? l2[i][j]:l3[i][j-1]
+    //             }
+    //         }
+    //     }
+    // }
+    l2=l.map(x => x.map(y => Math.floor(y*90)/100))
+       for (var i=0;i<l.length;i++){
+        for (var j=0;j<l[i].length;j++){
+            m=1
+            if (points.length>1){
+            var m=((Math.sqrt(((points[0][0]-j)**2)+((points[0][1]-i)**2))*10))/500
+            for (var p=0;p<points.length;p++){
+                distance=((Math.sqrt(((points[p][0]-j)**2)+((points[p][1]-i)**2))*10))/500
+
+                 m=(distance<m)?distance:m
+            
+            }    
+            }
+            l2[i][j]=(m>0)? m:0.0000001
+        }
+    }
+
+    return l2
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+function schre(){
+    window.location.href("https://www.sst.edu.sg");
+}
--- script.js
+++ script.js
@@ -1,190 +1,105 @@
-let did=0
-let dob=new Date(2006,03,28)
-var running=true
+"use strict"
+let carat="█"
+let prompt;
+var currentdirectory=[]
+let filesystem={
+    "skills" : {
+                "proficient.txt" : "proficient.txt",
+                "learning.txt" : "learning.txt",
+                "want_to_learn.txt":"want_to_learn.txt"
+                
+                },
+    "projects":{
+                "this.txt":"this website"
+                }
+}
+let commands={
+    'ls':lis,
+    'help':h,
+    'cd':cd
+}
 function load(){
+    prompt=document.getElementById("prompt").innerHTML;
+    setInterval(() => {
+        var c=document.getElementById("cursorblink")
+        if (c.innerHTML==carat){
+            c.innerHTML=""
+        }else{
+            c.innerHTML=carat
+        }
+    }, 800);
+    document.getElementsByClassName('command')[document.getElementsByClassName('command').length-1].focus();
+}
+function runcommand(){
+    var t=document.getElementsByClassName('command')
+    t[t.length-1].setAttribute("contenteditable", false);
+    let command=t[t.length-1].innerHTML.replace(/[<]br[^>]*[>]/gi,"");
+    var output="<span style='color:red;'>OOP command not found</span>"
+    for (var key in commands){
+        console.log(command.split(' ')[0])
+        if (command.split(' ')[0]==key){
+            output=commands[key](command.substring(key.length+1))
+        }
+
+    }
     
-    document.getElementById("school").innerHTML="School of Science and Technology Singapore"
-    if (window.innerWidth>600){
-        desktop()
-    }else{
-        mobile()
-    }
-    draw()
-}
-function mobile(){
-    document.getElementById("mobile-navbar").style.display="block"
-    document.getElementById("mobile-navbar").style.listStyleType = "none";
-    items=document.getElementsByClassName("navitem")
-    start=0
-    for (let i=0;i<items.length;i++){
-        items[i].style.float="none"
-        items[i].style.display="none"
-    }
+
+
+
     
-    document.getElementById("mm").style.display="block"
-    document.getElementById("mm").style.float="right"
-}
-function show(){
-    // console.log("hit")
-    items=document.getElementsByClassName("navitem")
-    if (items[1].style.display=="none"){
-        for (let i=0;i<items.length;i++){
-        items[i].style.display="block"
-        }
-        start=0
-        id=setInterval(frame,10)
-        function frame(){
-        if (start>=2){
-            clearInterval(id)
-        }
-        start+=0.1
-        // console.log(start)
-        for (let i=0;i<items.length;i++){
-            items[i].style.top=toString(start)+"rem"
-            // console.log(items[i].style.height)
-        }
-        }
-    }else{
-        for (let i=0;i<items.length;i++){
-            items[i].style.display="none"
-            }
-            document.getElementById("mm").style.display="block"
-    }
-}
+    document.getElementById("history").innerHTML=document.getElementById("history").innerHTML.concat('<br>',output,(output.length>0)? '<br>' :'' , prompt)
+    document.getElementsByClassName('command')[document.getElementsByClassName('command').length-1].focus();
+    var dir=document.getElementsByClassName('dir')[document.getElementsByClassName('dir').length-1]
+    dir.innerHTML=(currentdirectory.length>0) ? currentdirectory[currentdirectory.length-1]:"root"
+    
 
-function desktop(){
-    items=document.getElementsByClassName("navitem")
-    for (let i=0;i<items.length;i++){
-        items[i].style.display="block"
-        items[i].style.float="right"
-    }
-    document.getElementById("mm").style.display="none"
 
 }
-
-
-
-
-function draw(){
-    var canvas = document.querySelector("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.canvas.width  =innerWidth;
-    ctx.canvas.height = innerHeight;
-    last=0
-    var x=0
-    var y=0
-    var s=10
-    var arr = Array(Math.floor((innerHeight/s)+2)).fill().map(() => Array(Math.floor((innerWidth/s)+2)).fill().map(() => 1));
-    var ps=[]
-    ps=[[Math.floor(Math.random()*arr.length),Math.floor( Math.random()*arr[0].length)]]
-    for (var i=0;i<Math.floor(Math.random()*arr.length)+10;i++){
-        ps.push([Math.floor(Math.random()*arr[0].length),Math.floor( Math.random()*arr.length)])
-    }
-    var directions=[]
-    for (var i=0;i<ps.length;i++){
-        directions.push([Math.floor((Math.random()-0.5)*30)/10,Math.floor((Math.random()-0.5)*30)/10])
-    }
-    // document.addEventListener("click", addpos);
-    window.requestAnimationFrame(gameLoop);
-    // function addpos(event){
-    //     // arr[Math.floor(event.clientY/s)][Math.floor(event.clientX/s)]=1
-    //     // console.log(arr)
-    //     x=Math.floor(event.clientX/s)
-    //     y=Math.floor(event.clientY/s)
-    //     ps.push([x,y])
-
-    // }
-    function gameLoop(ts){
-        let time=new Date();
-        document.getElementById("age").innerHTML = Math.floor((time.getTime()-dob.getTime())/315.36)/100000000
-
-
-        if (last != Math.floor(ts/50)){
-            last=Math.floor(ts/50)
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "hsl(239, 100%, 17%)";
-        ctx.fillRect(0,0,canvas.width, canvas.height)
-
-        arr=generation(ps,arr)
+function lis (x){
+    var current=filesystem
+    for (var i=0;i<currentdirectory.length;i++){
+        current=filesystem[currentdirectory[i]]
         
-        for (var i=0;i<arr.length;i++){
-            for (var j=0;j<arr[i].length;j++){
-                if (arr[i][j]>0){
-                ctx.fillStyle = `hsl(
-                    ${/*Math.floor(arr[i][j]*255)*/ 239},
-                    ${(100)-Math.floor(arr[i][j]*(10*s)) }%,
-                    ${(40)+Math.floor(arr[i][j]*2.1*s)}%)`;
-                // ctx.fillRect((j-1)*s,(i-1)*s,s,s)
-                ctx.beginPath()
-                ctx.arc((j-1)*s, (i-1)*s, s/2, 0, 2 * Math.PI, false);
-                ctx.fill()
-                ctx.closePath();
-            }
-            }
-        }
-        for (var i=0;i<directions.length;i++){
-            ps[i]=[ps[i][0]+directions[i][0],ps[i][1]+directions[i][1]]
-            if (ps[i][0]-2<0 || ps[i][0]+2>arr[0].length){
-                directions[i][0]=directions[i][0]*-1
-            }
-            if (ps[i][1]-2<0 || ps[i][1]+2>arr.length){
-                directions[i][1]=directions[i][1]*-1
-            }
-            
-        }
-
-
-    }        
-        window.requestAnimationFrame(gameLoop);
     }
+    console.log(current)
+    var final=[]
+    for (var key in current){
+        final.push(key)
+    }
+    return final.join('<br>')
 }
-
-function generation(points,l){
-    // l3=l.map(x => x.map(y => Math.floor(y*90)/100))
-    // l2=l.map(x => x.map(y => Math.floor(y*90)/100))
-    // for (var i=0;i<l.length;i++){
-    //     for (var j=0;j<l[i].length;j++){
-    //         if (l[i][j]>0){
-    //             if (i+1<l.length){
-    //                 l2[i+1][j]=(l2[i][j]>l3[i+1][j]) ? l2[i][j]:l3[i+1][j]
-    //             }
-    //             if (i-1>0){
-    //                 l2[i-1][j]=(l2[i][j]>l3[i-1][j]) ? l2[i][j]:l3[i-1][j]
-    //             }
-    //             if (j+1<l[i].length){
-    //                 l2[i][j+1]=(l2[i][j]>l3[i][j+1]) ? l2[i][j]:l3[i][j+1]
-    //             }
-    //             if (j-1>0){
-    //                 l2[i][j-1]=(l2[i][j]>l3[i][j-1]) ? l2[i][j]:l3[i][j-1]
-    //             }
-    //         }
-    //     }
-    // }
-    l2=l.map(x => x.map(y => Math.floor(y*90)/100))
-       for (var i=0;i<l.length;i++){
-        for (var j=0;j<l[i].length;j++){
-            m=1
-            if (points.length>1){
-            var m=((Math.sqrt(((points[0][0]-j)**2)+((points[0][1]-i)**2))*10))/500
-            for (var p=0;p<points.length;p++){
-                distance=((Math.sqrt(((points[p][0]-j)**2)+((points[p][1]-i)**2))*10))/500
-
-                 m=(distance<m)?distance:m
-            
-            }    
-            }
-            l2[i][j]=(m>0)? m:0.0000001
-        }
+function h(x){
+    var final=''
+    for (var keys in commands){
+        final=final.concat('\t',keys)
+    }
+    return  final
+}
+function cd(x){
+    var current=filesystem
+    for (var i=0;i<currentdirectory.length;i++){
+        current=filesystem[currentdirectory[i]]
+        
     }
 
-    return l2
-}
+    x = x.replace(/[<]br[^>]*[>]/gi,"");
+    console.log(x in current)
+    if (x=='..'){
+        if (currentdirectory.length >0){
+            currentdirectory.pop()
+        }
+    }else{
+        if (x in current){
+            if (current[x].constructor == Object){
+                currentdirectory.push(x)
+                console.log(currentdirectory)
+            }else{
+                return "not dir"
+            }
 
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
-function schre(){
-    window.location.href("https://www.sst.edu.sg");
+        } else{
+            return "not in"
+        }
+    }
+    return ''
 }
