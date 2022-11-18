777f6de7417bcaa2fa393953895613adb0ea5f1a
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2022-11-18 11:30:02 +0800

    Update normal.js

--- normal.js
+++ normal.js
@@ -10,10 +10,10 @@ function getstuff(directory,cwd=[],depth=0){
 	console.log(directory)
 	for (const fn in directory){
 		console.log(fn)
-		output+=`<div class="col"><div class="header"  onclick="toggle('${cwd[cwd.length-1]}${fn}${depth+1}')" style="width:fit-content">`+
+		output+=`<div class="col"><div class="header"  onclick="toggle('${cwd[cwd.length-1]}${fn}${depth+1}')" style="width:fit-content;max-width:80ch">`+
 			`<span style='color:${(typeof directory[fn]==='string')?"#ff0000":"#00ff00"}'><h${depth+1}>${fn}</h${depth+1}></span>`+
 			`</div>`
-		output+=`<div id='${cwd[cwd.length-1]}${fn}${depth+1}'style='border-left: 5px solid #00ff00;display:none;margin-left:1rem;padding-left:1rem;border-radius: 20px 0px 0px 20px; height: fit-content; width: fit-content;'>`
+		output+=`<div id='${cwd[cwd.length-1]}${fn}${depth+1}'style='border-left: 5px solid #00ff00;display:none;margin-left:1rem;padding-left:1rem; height: fit-content; width: fit-content;'>`
 		if (typeof directory[fn]==='string'){
 			
 				output+=getfile(cwd,directory[fn])
