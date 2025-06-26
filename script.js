eecd93d6a15620a79186b614af32e262b1f1eeca
jer123se12 <jerickseng06@gmail.com>
2025-06-26 23:50:51 +0800

    added

---
+++ script.js
@@ -0,0 +1,43 @@
+const sections=[
+"about",
+"projects",
+"events",
+"experience",
+"achievements"]
+let nav;
+function load(){
+    nav=document.getElementsByTagName("nav")[0].getElementsByTagName("a")
+document.onscroll=scroll
+    setCorrectTab()
+}
+function selectNav(position){
+    for (let i=0;i<nav.length;i++){
+        if (i==position){
+            nav[i].className="selected"
+        }else{
+            nav[i].className=""
+        }
+    }
+}
+function setCorrectTab(){
+
+        let selected=false;
+        for (let i=0;i<sections.length;i++){
+            let section=document.getElementById(sections[i])
+            let scroll=window.scrollY
+                +(window.innerHeight/2);
+           if (section.offsetTop-scroll>0) {
+               selectNav(i-1)
+               selected=true;
+               console.log("selected")
+               break
+           }
+           
+        }
+        if (!selected){
+            selectNav(sections.length-1)
+        }
+}
+function scroll(event){
+    setCorrectTab()
+}
